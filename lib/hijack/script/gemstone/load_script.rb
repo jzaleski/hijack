1d3e98152a05d0f12865d8aea6ab81b81e83ab94
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-06-08 11:45:39 -0400

    Update "load_script" to grab ammo from "ammo_container"

--- lib/hijack/script/gemstone/load_script.rb
+++ lib/hijack/script/gemstone/load_script.rb
@@ -3,13 +3,14 @@ require 'hijack/script/base_script'
 class LoadScript < BaseScript
 
   def validate_args(args)
-    args.length == 2 || (config_weapon && config_ammo)
+    args.length == 3 || (config_weapon && config_ammo && config_ammo_container)
   end
 
   def run(args)
     weapon = args[0] || config_weapon
     ammo = args[1] || config_ammo
-    puts "get 1 #{ammo}"
+    ammo_container = args[2] || config_ammo_container
+    puts "get 1 #{ammo} from my #{ammo_container}"
     puts "load my #{weapon}"
     puts 'stance def'
   end
@@ -22,6 +23,10 @@ class LoadScript < BaseScript
 
   def config_ammo
     @config[:ammo]
+  end
+
+  def config_ammo_container
+    @config[:ammo_container]
   end
 
 end
