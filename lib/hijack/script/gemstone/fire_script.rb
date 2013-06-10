e8068a7dad12dadbc54efbb14971fbfbd04a0ea4
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-06-10 13:19:42 -0400

    Create "NockScript" and rework "FireScript" to take the "weapon_type" into account

--- lib/hijack/script/gemstone/fire_script.rb
+++ lib/hijack/script/gemstone/fire_script.rb
@@ -2,13 +2,24 @@ require 'hijack/script/base_script'
 
 class FireScript < BaseScript
 
+  def validate_args(args)
+    args.length == 1 || config_weapon_type
+  end
+
   def run(args)
-    puts 'kneel'
+    using_bow = (args[0] || config_weapon_type) !~ /\Acrossbow\Z/
     puts 'stance off'
+    puts 'kneel' unless using_bow
     puts 'fire'
-    sleep 2.5
+    sleep using_bow ? 5.25 : 2.25
     puts 'stance def'
-    puts 'stand'
+    puts 'stand' unless using_bow
+  end
+
+  protected
+
+  def config_weapon_type
+    @config[:weapon_type]
   end
 
 end
