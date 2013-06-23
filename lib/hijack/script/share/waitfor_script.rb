5d50c23103928e8f53b140f93d4fff3e6b38bec9
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-06-22 23:35:10 -0400

    Add TODO to "waitfor_script.rb"

--- lib/hijack/script/share/waitfor_script.rb
+++ lib/hijack/script/share/waitfor_script.rb
@@ -6,6 +6,9 @@ class WaitforScript < BaseScript
     args.length >= 2
   end
 
+  # TODO: this works fine for all commands except for: exit/quit. For these
+  # commands the socket is closed, but the screen session is not torn down
+  # because the client is waiting on a read
   def run(args)
     @bridge.callback_manager.add_match(
       args[0],
