78d73a7432379bd3f3831fbc3899597e9a8c61df
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-06-29 22:18:23 -0400

    Remove inaccurate comment and send the commands from the "waitfor_script" as one instead of many

--- lib/hijack/script/share/waitfor_script.rb
+++ lib/hijack/script/share/waitfor_script.rb
@@ -6,13 +6,10 @@ class WaitforScript < BaseScript
     args.length >= 2
   end
 
-  # TODO: this works fine for all commands except for: exit/quit. For these
-  # commands the socket is closed, but the screen session is not torn down
-  # because the client is waiting on a read
   def run(args)
     @bridge.callback_manager.add_match(
       args[0],
-      lambda {args[1..-1].each {|arg| puts arg}}
+      lambda {puts args[1..-1].join(' ')}
     )
   end
 
