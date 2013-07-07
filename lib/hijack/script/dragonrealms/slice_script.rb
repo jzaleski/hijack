e1cb585d9bab789aeb30442e7e3a5d197ac824a8
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-07-07 00:26:44 -0400

    Rework "{slice,thrust}_script.rb" to take roundtime into account (these two scripts should be DRY'd at some point)

--- lib/hijack/script/dragonrealms/slice_script.rb
+++ lib/hijack/script/dragonrealms/slice_script.rb
@@ -2,20 +2,40 @@ require 'hijack/script/base_script'
 
 class SliceScript < BaseScript
 
+  IN_ROUNDTIME = '...wait'
+  ROUNDTIME = 'Roundtime'
+
+  ATTACK_PATTERN = [
+    IN_ROUNDTIME,
+    ROUNDTIME,
+  ].join('|')
+
   def run(args)
     num_iterations = (args[0] || 1).to_i
     loop do
       num_iterations.times do |i|
-        puts 'feint'
-        sleep 2.5
-        puts 'slice'
-        sleep 3.5
-        puts 'draw'
-        sleep 3.5
-        puts 'chop'
-        sleep 4.5
+        attack('feint', 2.5)
+        attack('slice', 3.5)
+        attack('draw', 3.5)
+        attack('chop', 4.5)
       end
       wait_for_match 'You feel fully rested.'
+    end
+  end
+
+  private
+
+  def attack(attack_type, success_sleep_time)
+    loop do
+      match = wait_for_match(
+        ATTACK_PATTERN,
+        attack_type
+      )
+      if match == ROUNDTIME
+        sleep success_sleep_time
+        break
+      end
+      sleep 1
     end
   end
 
--- lib/hijack/script/dragonrealms/thrust_script.rb
+++ lib/hijack/script/dragonrealms/thrust_script.rb
@@ -2,20 +2,40 @@ require 'hijack/script/base_script'
 
 class ThrustScript < BaseScript
 
+  IN_ROUNDTIME = '...wait'
+  ROUNDTIME = 'Roundtime'
+
+  ATTACK_PATTERN = [
+    IN_ROUNDTIME,
+    ROUNDTIME,
+  ].join('|')
+
   def run(args)
     num_iterations = (args[0] || 1).to_i
     loop do
       num_iterations.times do |i|
-        puts 'feint'
-        sleep 2.5
-        puts 'jab'
-        sleep 2.5
-        puts 'thrust'
-        sleep 3.5
-        puts 'lunge'
-        sleep 4.5
+        attack('feint', 2.5)
+        attack('jab', 2.5)
+        attack('thrust', 3.5)
+        attack('lunge', 4.5)
       end
       wait_for_match 'You feel fully rested.'
+    end
+  end
+
+  private
+
+  def attack(attack_type, success_sleep_time)
+    loop do
+      match = wait_for_match(
+        ATTACK_PATTERN,
+        attack_type
+      )
+      if match == ROUNDTIME
+        sleep success_sleep_time
+        break
+      end
+      sleep 1
     end
   end
 
