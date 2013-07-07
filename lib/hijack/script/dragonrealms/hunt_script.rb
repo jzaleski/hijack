73359e2c2b35c333ad0b97df3d3dd36e60cd3f3b
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-07-07 00:32:38 -0400

    Rework "hunt_script.rb" to take roundtime into account

--- lib/hijack/script/dragonrealms/hunt_script.rb
+++ lib/hijack/script/dragonrealms/hunt_script.rb
@@ -2,9 +2,24 @@ require 'hijack/script/base_script'
 
 class HuntScript < BaseScript
 
+  HUNT_SUCCESS = 'You take note of all the tracks'
+  IN_ROUNDTIME = '...wait'
+
+  HUNT_PATTERN = [
+    HUNT_SUCCESS,
+    IN_ROUNDTIME,
+  ].join('|')
+
   def run(args)
     loop do
-      puts 'hunt'
+      loop do
+        match = wait_for_match(
+          HUNT_PATTERN,
+          'hunt'
+        )
+        break if match == HUNT_SUCCESS
+        sleep 1
+      end
       # the learning timeout is 75s, wait a few extra seconds for good measure
       sleep 80
     end
