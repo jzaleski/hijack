71eccbfa4eaedb5e4536bea5908be67399bd22e1
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-07-15 22:02:38 -0400

    Add additional success cases to "hide_script.rb"

--- lib/hijack/script/dragonrealms/hide_script.rb
+++ lib/hijack/script/dragonrealms/hide_script.rb
@@ -2,11 +2,22 @@ require 'hijack/script/base/base_dragonrealms_script'
 
 class HideScript < BaseDragonrealmsScript
 
-  HIDE_SUCCESS = 'You melt into'
+  ALREADY_HIDDEN = 'already hidden'
+  HIDE_SUCCESS_1 = 'You melt into'
+  HIDE_SUCCESS_2 = 'You blend in'
   NOT_HIDDEN = 'you are not hidden'
   UNHIDE = 'You come out'
 
-  HIDE_PATTERN = HIDE_SUCCESS
+  HIDE_PATTERN = [
+    ALREADY_HIDDEN,
+    HIDE_SUCCESS_1,
+    HIDE_SUCCESS_2,
+  ].join('|')
+
+  HIDE_SUCCESSES = [
+    HIDE_SUCCESS_1,
+    HIDE_SUCCESS_2,
+  ]
 
   UNHIDE_PATTERN = [
     NOT_HIDDEN,
@@ -22,11 +33,11 @@ class HideScript < BaseDragonrealmsScript
     intersequence_sleep_time = \
       (args[0] || config_intersequence_sleep_time || 15).to_i
     loop do
-      wait_for_match(
+      match = wait_for_match(
         HIDE_PATTERN,
         'hide'
       )
-      sleep 6.5
+      sleep 6.5 if HIDE_SUCCESSES.include?(match)
       wait_for_match(
         UNHIDE_PATTERN,
         'unhide'
