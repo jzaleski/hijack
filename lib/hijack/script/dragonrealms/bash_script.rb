549b54eedd3ce1b310ca98f6795efc8a1e112e7a
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-07-15 22:05:06 -0400

    Create "bash_script.rb" and related test stub

---
+++ lib/hijack/script/dragonrealms/bash_script.rb
@@ -0,0 +1,51 @@
+require 'hijack/script/base/base_dragonrealms_script'
+
+class BashScript < BaseDragonrealmsScript
+
+  NO_TARGETS = 'There is nothing else'
+  ROUNDTIME = 'Roundtime'
+
+  ATTACK_PATTERN = [
+    NO_TARGETS,
+    ROUNDTIME,
+  ].join('|')
+
+  ATTACKS_AND_SLEEP_TIMES = [
+    ['feint', 2.5],
+    ['bash', 4.5],
+    ['sweep', 4.5],
+    ['draw', 3.5],
+    ['swing', 3.5],
+  ]
+
+  def run(args)
+    intersequence_sleep_time = \
+      (args[0] || config_intersequence_sleep_time || 30).to_i
+    loop do
+      ATTACKS_AND_SLEEP_TIMES.each do |attack, sleep_time|
+        loop do
+          match = wait_for_match(
+            ATTACK_PATTERN,
+            attack
+          )
+          case match
+            when NO_TARGETS
+              sleep 15
+              next
+            when ROUNDTIME
+              sleep sleep_time
+              break
+          end
+        end
+      end
+      sleep intersequence_sleep_time
+    end
+  end
+
+  private
+
+  def config_intersequence_sleep_time
+    @config[:bash_intersequence_sleep_time]
+  end
+
+end
---
+++ spec/lib/hijack/script/dragonrealms/bash_script_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+require 'hijack/script/dragonrealms/bash_script'
+
+describe BashScript do; end
