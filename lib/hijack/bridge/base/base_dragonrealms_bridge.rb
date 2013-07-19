1f5f7f144c446cdcfb125a6c9ddf6bcfd9bb2fe7
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-07-19 15:54:26 +0000

    Handle new-lines and bold-text a little more intelligently

--- lib/hijack/bridge/base/base_dragonrealms_bridge.rb
+++ lib/hijack/bridge/base/base_dragonrealms_bridge.rb
@@ -3,8 +3,12 @@ require 'hijack/bridge/base/base_simutronics_bridge'
 class BaseDragonrealmsBridge < BaseSimutronicsBridge
 
   def gets
-    line = super
-    line.include?('***') ? '' : line
+    # strip leading/trailing carriage return characters
+    line = super.chomp
+    # ensure that lines w/ bold characters switch back to normal-text
+    line << "\e[0m" if line.include?('***')
+    # append a new-line before returning to the caller
+    "#{line}\n"
   end
 
 end
