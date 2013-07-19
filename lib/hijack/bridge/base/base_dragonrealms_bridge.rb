3e4492586bfe87320a6a0f06e056762682714ac4
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-07-19 14:18:56 -0400

    Add comments to indicate why it is necessary to change the font for certain lines + clean-up

--- lib/hijack/bridge/base/base_dragonrealms_bridge.rb
+++ lib/hijack/bridge/base/base_dragonrealms_bridge.rb
@@ -2,12 +2,19 @@ require 'hijack/bridge/base/base_simutronics_bridge'
 
 class BaseDragonrealmsBridge < BaseSimutronicsBridge
 
+  PROBLEM_PATTERNS = [
+    '***',
+    'Log-on system converted',
+  ]
+
   def gets
     # strip leading/trailing carriage return characters
     line = super.chomp
-    # ensure that lines w/ bold characters switch back to normal-text
-    line << "\e[0m" if line.include?('***')
-    # append a new-line before returning to the caller
+    # ensure that lines matching any of the problem patterns a switch back to
+    # normal (non-bold) text
+    line << "\e[0m" if PROBLEM_PATTERNS.any? {|pattern| line.include?(pattern)}
+    # make sure to append a new-line to the end of the output as it was likely
+    # stripped off above
     "#{line}\n"
   end
 
