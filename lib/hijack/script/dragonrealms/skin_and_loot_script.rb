3f7e1435b66aef3b7749376438ba58c9cc326a5d
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-07-19 22:29:31 -0400

    Create "skin_and_loot_script.rb" and related test stub

---
+++ lib/hijack/script/dragonrealms/skin_and_loot_script.rb
@@ -0,0 +1,80 @@
+require 'hijack/script/base/base_dragonrealms_script'
+
+class SkinAndLootScript < BaseDragonrealmsScript
+
+  DEAD_LONG = 'which appears dead'
+  DEAD_SHORT = '\(dead\)'
+  LOOT_SUCCESS = 'You search'
+  NO_CORPSE = 'I could not find'
+  OBVIOUS_EXITS = 'Obvious exits:'
+  OBVIOUS_PATHS = 'Obvious paths:'
+
+  LOOK_FAILURES = [
+    OBVIOUS_EXITS,
+    OBVIOUS_PATHS,
+  ]
+
+  LOOK_PATTERN = [
+    DEAD_LONG,
+    DEAD_SHORT,
+    OBVIOUS_EXITS,
+    OBVIOUS_PATHS,
+  ].join('|')
+
+  LOOT_PATTERN = [
+    LOOT_SUCCESS,
+    NO_CORPSE,
+  ].join('|')
+
+  def validate_args(args)
+    args.length >= 1 || config_skin_type
+  end
+
+  def run(args)
+    skin_type = args[0] || config_skin_type
+    loot_type = args[1] || config_loot_type || 'goods'
+    loop do
+      # anything to skin & loot?
+      match = wait_for_match(
+        LOOK_PATTERN,
+        'look'
+      )
+      # if there isn't anything to loot wait a bit and then try again
+      if LOOK_FAILURES.include?(match)
+        sleep 15
+        next
+      end
+      # skin
+      wait_for_match(
+        "#{skin_type} from|#{NO_CORPSE}",
+        'skin'
+      )
+      # wait a few seconds before attempting to drop the skin
+      sleep 3
+      # drop the skin
+      wait_for_match(
+        'You drop',
+        "drop #{skin_type}"
+      )
+      # loot the creature
+      wait_for_match(
+        LOOT_PATTERN,
+        "loot #{loot_type}"
+      )
+      # wait a few additional seconds on loot-success before returning to the
+      # main loop (give the creature some time to decay)
+      sleep 5
+    end
+  end
+
+  private
+
+  def config_skin_type
+    @config[:skin_type]
+  end
+
+  def config_loot_type
+    @config[:loot_type]
+  end
+
+end
---
+++ spec/lib/hijack/script/dragonrealms/skin_and_loot_script_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+require 'hijack/script/dragonrealms/skin_and_loot_script'
+
+describe SkinAndLootScript do; end
