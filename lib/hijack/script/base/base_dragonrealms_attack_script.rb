dde8b9633e293a431886a4a03844e94f0d709918
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-07-21 00:21:12 -0400

    Handle case when attacking while not at melee range

--- lib/hijack/script/base/base_dragonrealms_attack_script.rb
+++ lib/hijack/script/base/base_dragonrealms_attack_script.rb
@@ -2,6 +2,7 @@ require 'hijack/script/base/base_dragonrealms_script'
 
 class BaseDragonrealmsAttackScript < BaseDragonrealmsScript
 
+  BEGIN_TO_ADVANCE = 'You begin to advance'
   NO_TARGETS = 'There is nothing else'
 
   def run(args)
@@ -10,10 +11,12 @@ class BaseDragonrealmsAttackScript < BaseDragonrealmsScript
       combat_sequence.each do |attack, sleep_time|
         attack_pattern = "[Yy]ou #{attack}"
         match = wait_for_match(
-          "#{attack_pattern}|#{NO_TARGETS}",
+          "#{attack_pattern}|#{BEGIN_TO_ADVANCE}|#{NO_TARGETS}",
           attack
         )
         case match
+          when BEGIN_TO_ADVANCE
+            break
           when Regexp.new(attack_pattern)
             sleep sleep_time
           when NO_TARGETS
