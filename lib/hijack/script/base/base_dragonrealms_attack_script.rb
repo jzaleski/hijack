c77f5cb395ad09946e417c3e39e15401f26264e6
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-07-21 00:57:48 -0400

    Handle case when not facing a target in "base_dragonrealms_attack_script.rb"

--- lib/hijack/script/base/base_dragonrealms_attack_script.rb
+++ lib/hijack/script/base/base_dragonrealms_attack_script.rb
@@ -4,6 +4,7 @@ class BaseDragonrealmsAttackScript < BaseDragonrealmsScript
 
   BEGIN_TO_ADVANCE = 'You begin to advance'
   NO_TARGETS = 'There is nothing else'
+  TURN_TO_FACE = 'You turn to face'
 
   def run(args)
     intersequence_sleep_time = (args[0] || 30).to_i
@@ -11,15 +12,13 @@ class BaseDragonrealmsAttackScript < BaseDragonrealmsScript
       combat_sequence.each do |attack, sleep_time|
         attack_pattern = "[Yy]ou #{attack}"
         match = wait_for_match(
-          "#{attack_pattern}|#{BEGIN_TO_ADVANCE}|#{NO_TARGETS}",
+          "#{attack_pattern}|#{BEGIN_TO_ADVANCE}|#{NO_TARGETS}|#{TURN_TO_FACE}",
           attack
         )
         case match
-          when BEGIN_TO_ADVANCE
-            break
           when Regexp.new(attack_pattern)
             sleep sleep_time
-          when NO_TARGETS
+          when Regexp.new("#{BEGIN_TO_ADVANCE}|#{NO_TARGETS}|#{TURN_TO_FACE}")
             break
         end
       end
