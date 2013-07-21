820e4aef6a5aef605af999f554c906bd7903e822
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-07-21 00:42:35 -0400

    Simplify logic in "tm_script.rb"

--- lib/hijack/script/dragonrealms/tm_script.rb
+++ lib/hijack/script/dragonrealms/tm_script.rb
@@ -5,11 +5,11 @@ class TmScript < BaseDragonrealmsScript
   ALREADY_PREPARED = 'You have already fully'
   ALREADY_TARGETTED = 'Your target pattern is'
   CASTED = 'You gesture'
+  NO_SPELL = "You aren't preparing a spell"
   NO_TARGETS = 'There is nothing else'
   PREPARING_SPELL = 'You raise an'
   RELEASE = 'You let your concentration lapse'
   TARGETTING = 'You begin to weave'
-  TARGETTING_COMPLETE = 'Your formation of a targeting pattern'
   TARGET_DEAD = 'Your target pattern dissipates'
 
   CAST_PATTERN = [
@@ -22,7 +22,10 @@ class TmScript < BaseDragonrealmsScript
     PREPARING_SPELL,
   ].join('|')
 
-  RELEASE_PATTERN = RELEASE
+  RELEASE_PATTERN = [
+    NO_SPELL,
+    RELEASE,
+  ].join('|')
 
   TARGET_FAILURES = [
     NO_TARGETS,
@@ -36,24 +39,21 @@ class TmScript < BaseDragonrealmsScript
     TARGET_DEAD,
   ].join('|')
 
-  TARGETTING_COMPLETE_PATTERN = [
-    TARGETTING_COMPLETE,
-    TARGET_DEAD,
-  ].join('|')
-
   def validate_args(args)
-    args.length == 1 || config_spell
+    args.length <= 1 ||
+    (config_spell && config_mana)
   end
 
   def run(args)
     spell = args[0] || config_spell
     mana = args[1] || config_mana
+    prep_command = "prep #{spell} #{mana}".rstrip
     loop do
       wait_for_match(
         PREP_PATTERN,
-        "prep #{spell} #{mana}".rstrip
+        prep_command
       )
-      sleep 1
+      sleep 1.5
       match = wait_for_match(
         TARGET_PATTERN,
         'target'
@@ -66,22 +66,11 @@ class TmScript < BaseDragonrealmsScript
         sleep 15
         next
       end
-      wait_for_match(
-        TARGETTING_COMPLETE_PATTERN
-      )
-      if match == TARGET_DEAD
-        wait_for_match(
-          RELEASE_PATTERN,
-          'release'
-        )
-        sleep 15
-        next
-      end
+      sleep 7.5
       wait_for_match(
         CAST_PATTERN,
         'cast'
       )
-      sleep 1
     end
   end
 
