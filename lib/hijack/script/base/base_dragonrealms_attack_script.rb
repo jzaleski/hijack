07e69f8e70d0be71521d848ba52230e0741fb1c6
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-07-27 16:08:49 +0000

    Add additional failure case to "base_dragonrealms_attack_script.rb" + clean-up

--- lib/hijack/script/base/base_dragonrealms_attack_script.rb
+++ lib/hijack/script/base/base_dragonrealms_attack_script.rb
@@ -3,8 +3,16 @@ require 'hijack/script/base/base_dragonrealms_script'
 class BaseDragonrealmsAttackScript < BaseDragonrealmsScript
 
   BEGIN_TO_ADVANCE = 'You begin to advance'
+  MUST_BE_CLOSER = 'You must be closer'
   NO_TARGETS = 'There is nothing else'
   TURN_TO_FACE = 'You turn to face'
+
+  ATTACK_FAILURES = [
+    BEGIN_TO_ADVANCE,
+    MUST_BE_CLOSER,
+    NO_TARGETS,
+    TURN_TO_FACE,
+  ]
 
   def run(args)
     intersequence_sleep_time = (args[0] || 30).to_i
@@ -12,13 +20,13 @@ class BaseDragonrealmsAttackScript < BaseDragonrealmsScript
       combat_sequence.each do |attack, sleep_time, attack_pattern|
         attack_pattern ||= "[Yy]ou #{attack}"
         match = wait_for_match(
-          "#{attack_pattern}|#{BEGIN_TO_ADVANCE}|#{NO_TARGETS}|#{TURN_TO_FACE}",
+          "#{attack_pattern}|#{ATTACK_FAILURES.join('|')}",
           attack
         )
         case match
           when Regexp.new(attack_pattern)
             sleep sleep_time
-          when Regexp.new("#{BEGIN_TO_ADVANCE}|#{NO_TARGETS}|#{TURN_TO_FACE}")
+          else
             break
         end
       end
