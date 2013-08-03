9f9c90c497668ed6418a762fe59ce35b073289c6
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-08-03 02:02:48 +0000

    Add the ability to observe multiple times in "predict_script.rb"

--- lib/hijack/script/dragonrealms/predict_script.rb
+++ lib/hijack/script/dragonrealms/predict_script.rb
@@ -79,6 +79,8 @@ class PredictScript < BaseDragonrealmsScript
   ]
 
   def run(args)
+    num_observes = [1, (args[0] || config_num_observes).to_i].max
+    successful_observes = 0
     loop do
       # cast helper spells
       SPELLS.each do |spell|
@@ -91,51 +93,67 @@ class PredictScript < BaseDragonrealmsScript
           'cast'
         )
       end
-      # observe, align, predict future & analyze bonuses/curses
-      loop do
-        # shuffle the list each iteration of the main-loop to avoid hot-spots
-        OBJECTS.shuffle.each do |object|
-          match = wait_for_match(
-            OBSERVE_PATTERN,
-            "observe #{object}"
-          )
-          case match
-            # too soon
-            when YOU_HAVE_NOT_PONDERED
-              sleep 30
-              break
-            # success
-            when Regexp.new("#{YOU_LEARNED}|#{YOU_STILL_LEANRED}")
-              # 10 seconds is the minimum roundtime imposed
-              sleep 10
-              # try every alignment
-              ALIGNMENTS.each do |alignment|
-                wait_for_match(
-                  ALIGN_PATTERN,
-                  "align #{alignment}"
-                )
-                # always 2 seconds roundtime
-                sleep 2
-                match = wait_for_match(
-                  PREDICT_FUTURE_PATTERN,
-                  'predict future'
-                )
-                # handle the variable roundtime
-                sleep match == REMAINS_A_DARK_MYSTERY ? 5 : 10
-              end
-              # analyze
-              wait_for_match(
-                PREDICT_ANALYZE_PATTERN,
-                'predict analyze'
-              )
-              # sleep long enough to cover the roundtime and a sizable chunk, if
-              # not the entire cooldown time, before restarting
+      # observe, align, predict then analyze bonuses/curses - when observing
+      # multiple times it is unclear whether we want to use the same object so
+      # that the same pools are built up with each iteration or shuffle the list
+      # so that we can potentially have successful predictions across a variety
+      # of pools (for now, the objects are shuffled)
+      OBJECTS.shuffle.each do |object|
+        match = wait_for_match(
+          OBSERVE_PATTERN,
+          "observe #{object}"
+        )
+        case match
+          # too soon
+          when YOU_HAVE_NOT_PONDERED
+            sleep 30
+            break
+          # success
+          when Regexp.new("#{YOU_LEARNED}|#{YOU_STILL_LEANRED}")
+            # increment the success counter (first)
+            successful_observes += 1
+            # short-circuit if there is more observing to do
+            if successful_observes < num_observes
               sleep 120
               break
-          end
+            end
+            # if we've made it this far it is safe to reset the counter
+            successful_observes = 0
+            # 10 seconds is the minimum roundtime imposed
+            sleep 10
+            # try every alignment
+            ALIGNMENTS.each do |alignment|
+              wait_for_match(
+                ALIGN_PATTERN,
+                "align #{alignment}"
+              )
+              # always 2 seconds roundtime
+              sleep 2
+              match = wait_for_match(
+                PREDICT_FUTURE_PATTERN,
+                'predict future'
+              )
+              # handle the variable roundtime
+              sleep match == REMAINS_A_DARK_MYSTERY ? 5 : 10
+            end
+            # analyze
+            wait_for_match(
+              PREDICT_ANALYZE_PATTERN,
+              'predict analyze'
+            )
+            # sleep long enough to cover the roundtime and a sizable chunk, if
+            # not the entire cooldown time, before restarting
+            sleep 120
+            break
         end
       end
     end
+  end
+
+  private
+
+  def config_num_observes
+    @config[:predict_num_observes]
   end
 
 end
