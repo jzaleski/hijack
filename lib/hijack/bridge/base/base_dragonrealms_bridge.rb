409336f468c84ee4e4fa712df5b9e841f039c806
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-08-31 23:03:12 -0400

    Ensure that lines containing bold text switch back to normal (non-bold) text at the end of the line

--- lib/hijack/bridge/base/base_dragonrealms_bridge.rb
+++ lib/hijack/bridge/base/base_dragonrealms_bridge.rb
@@ -5,11 +5,6 @@ class BaseDragonrealmsBridge < BaseSimutronicsBridge
   SORRY_YOU_MAY_ONLY_TYPE_AHEAD = 'Sorry, you may only type ahead'
   WAIT = '\.\.\.wait'
 
-  PROBLEM_PATTERNS = [
-    '***',
-    'Log-on system converted',
-  ]
-
   RETRY_PATTERN = [
     SORRY_YOU_MAY_ONLY_TYPE_AHEAD,
     WAIT,
@@ -18,9 +13,9 @@ class BaseDragonrealmsBridge < BaseSimutronicsBridge
   def gets
     # strip trailing carriage return characters
     line = super.chomp
-    # ensure that lines matching any of the problem patterns a switch back to
-    # normal (non-bold) text
-    line << "\e[0m" if PROBLEM_PATTERNS.any? {|pattern| line.include?(pattern)}
+    # ensure that lines containing bold text switch back to normal (non-bold)
+    # text at the end of the line
+    line << "\e[0m" if line.match(/\e\[\d+m/)
     # determine whether or not we want to output the line (an empty-string will
     # not be output) and make sure to append a new-line to the end of the output
     # as it was likely stripped off above
