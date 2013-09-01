0c23008161a3c49db0ac459afcd54fc5c95ec817
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-08-31 22:27:51 -0400

    Add windows support for calculating the number of rows/columns

--- lib/hijack/manager/layout_manager.rb
+++ lib/hijack/manager/layout_manager.rb
@@ -1,14 +1,25 @@
 class LayoutManager
 
-  DEFAULT_NUM_COLS = 125
-  DEFAULT_NUM_ROWS = 35
-
   def self.num_cols
-    @@num_cols ||= (STDIN.tty? ? `stty size`.split.last : `tput cols`).to_i rescue DEFAULT_NUM_COLS
+    @@num_cols ||= begin
+      if RUBY_PLATFORM =~ /mingw32/
+        mode_parts = `mode con`.split
+        mode_parts[mode_parts.index('Columns:') + 1].to_i
+      else
+        (STDIN.tty? ? `stty size`.split.last : `tput cols`).to_i
+      end
+    end
   end
 
   def self.num_rows
-    @@num_rows ||= (STDIN.tty? ? `stty size`.split.first : `tput rows`).to_i rescue DEFAULT_NUM_ROWS
+    @@num_rows ||= begin
+      if RUBY_PLATFORM =~ /mingw32/
+        mode_parts = `mode con`.split
+        mode_parts[mode_parts.index('Lines:') + 1].to_i
+      else
+        (STDIN.tty? ? `stty size`.split.first : `tput rows`).to_i
+      end
+    end
   end
 
 end
