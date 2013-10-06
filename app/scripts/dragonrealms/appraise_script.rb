7d759269d00ae4a7669a09eca05e2ed1880748ea
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-10-06 14:16:58 -0400

    - "appraise_script.rb" will now successfully appraise creatures - "appraise_script.rb" will now loop

--- app/scripts/dragonrealms/appraise_script.rb
+++ app/scripts/dragonrealms/appraise_script.rb
@@ -2,6 +2,22 @@ require 'scripts/base/base_dragonrealms_script'
 
 class AppraiseScript < BaseDragonrealmsScript
 
+  ITEM_APPEARS_TO_BE = '%{item} appears to be'
+  ITEM_IS_A = '%{item} is a'
+  ITEM_IS_WORTH = '%{item} is worth'
+  ITEM_LOOKS_LIKE = '%{item} look[s]? like'
+  TAKING_STOCK_OF_ITS = 'Taking stock of its'
+  YOU_BEGIN_TO_CAREFULLY_STUDY = 'You begin to carefully study'
+
+  APPRAISE_SUCCESS_PATTERN = [
+    ITEM_APPEARS_TO_BE,
+    ITEM_IS_A,
+    ITEM_IS_WORTH,
+    ITEM_LOOKS_LIKE,
+    TAKING_STOCK_OF_ITS,
+    YOU_BEGIN_TO_CAREFULLY_STUDY,
+  ].join('|')
+
   APPRAISE_WHAT = 'Appraise what\?'
 
   APPRAISE_FAILURE_PATTERN = APPRAISE_WHAT
@@ -13,26 +29,24 @@ class AppraiseScript < BaseDragonrealmsScript
 
   def run(args)
     args = !args.empty? && args || config_args
-    args.each do |arg|
-      # strip off additional qualifiers, indices and descriptors
-      item = arg.split.last
-      appraise_success_pattern = [
-        "#{item} appears to be",
-        "#{item} is a",
-        "#{item} is worth",
-        "#{item} look[s]? like",
-        'You begin to carefully study',
-      ].join('|')
-      # because "appraise_success_pattern" is interpolated and "wait_for_match"
-      # will automatically convert the string to a Regexp, we need to handle 
-      # things a little differently
-      result = wait_for_match(
-        "#{appraise_success_pattern}|#{APPRAISE_FAILURE_PATTERN}",
-        "appraise #{arg}"
-      )
-      # only sleep on a successful appraise, don't worry about missing items or
-      # invalid selectors
-      sleep 10 if result.match(appraise_success_pattern)
+    interloop_sleep_time = 60
+    loop do
+      args.each do |arg|
+        # strip off any additional qualifiers, indices and/or descriptors
+        item = arg.split.last
+        # because we need to interpolate the pattern and "wait_for_match" will
+        # automatically convert the string to a Regexp, we need to handle things
+        # a little differently
+        appraise_success_pattern = APPRAISE_SUCCESS_PATTERN % {:item => item}
+        result = wait_for_match(
+          "#{appraise_success_pattern}|#{APPRAISE_FAILURE_PATTERN}",
+          "appraise #{arg}"
+        )
+        # only sleep on a successful appraise, don't worry about missing items
+        # or invalid selectors
+        sleep 10 if result.match(appraise_success_pattern)
+      end
+      sleep interloop_sleep_time
     end
   end
 
