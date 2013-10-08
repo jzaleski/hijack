feb11132f3a82b79cc6af0d352034ef172dd295e
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-10-07 22:19:17 -0400

    - handle an additional failure case in "appraise_script.rb"

--- app/scripts/dragonrealms/appraise_script.rb
+++ app/scripts/dragonrealms/appraise_script.rb
@@ -7,12 +7,14 @@ class AppraiseScript < BaseDragonrealmsScript
   ITEM_IS_A = '%{item} is a'
   ITEM_IS_WORTH = '%{item} is worth'
   ITEM_LOOKS_LIKE = '%{item} look[s]? like'
+  ITS_DEAD = "It's dead"
   TAKING_STOCK_OF_ITS = 'Taking stock of its'
   YOU_BEGIN_TO_CAREFULLY_STUDY = 'You begin to carefully study'
   YOU_CANNOT_APPRAISE_THAT = 'You cannot appraise that'
 
   APPRAISE_FAILURE_PATTERN = [
     APPRAISE_WHAT,
+    ITS_DEAD,
     YOU_CANNOT_APPRAISE_THAT,
   ].join('|')
 
