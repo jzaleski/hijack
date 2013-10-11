a7178146c64efbc4668de53882dd2c4a8af458ac
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-10-11 17:35:42 -0400

    Add tests for "AliasHelper.process"
    - add tests and clean-up AliasHelper itself to respect the uniform
      access principle

--- app/helpers/alias_helper.rb
+++ app/helpers/alias_helper.rb
@@ -2,13 +2,18 @@ class AliasHelper
 
   def initialize(config)
     @config = config
-    @aliases = config[:aliases] || {}
   end
 
   def process(command)
     alias_ = command.to_sym
-    return command unless @aliases.include?(alias_)
-    process(@aliases[alias_])
+    return command unless aliases.include?(alias_)
+    process(aliases[alias_])
+  end
+
+  private
+
+  def aliases
+    @config[:aliases] || {}
   end
 
 end
--- spec/helpers/alias_helper_spec.rb
+++ spec/helpers/alias_helper_spec.rb
@@ -3,8 +3,40 @@ require 'helpers/alias_helper'
 
 describe AliasHelper do
 
-  before do
-    @subject = AliasHelper.new({})
+  describe 'process' do
+
+    before do
+      @subject = AliasHelper.new({})
+    end
+
+    it 'will do nothing if there are no aliases configured' do
+      @subject.should_receive(:aliases).at_least(:once).
+        and_return({})
+
+      @subject.process('foo').should == 'foo'
+    end
+
+    it 'will do nothing if there are no aliases mapped to the command' do
+      @subject.should_receive(:aliases).at_least(:once).
+        and_return({:bar => 'biz'})
+
+      @subject.process('foo').should == 'foo'
+    end
+
+    it 'will replace an alias' do
+      @subject.should_receive(:aliases).at_least(:once).
+        and_return({:foo => 'bar'})
+
+      @subject.process('foo').should == 'bar'
+    end
+
+    it 'will recursively replace aliases' do
+      @subject.should_receive(:aliases).at_least(:once).
+        and_return({:foo => 'bar', :bar => 'biz'})
+
+      @subject.process('foo').should == 'biz'
+    end
+
   end
 
 end
