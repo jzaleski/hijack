8d4f4ab4fd68c510a129da1d86b91ed8e04942f8
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-11-25 21:27:21 -0500

    Add repeat support to "BaseGemstoneSpellupScript"

--- app/scripts/base/base_gemstone_spellup_script.rb
+++ app/scripts/base/base_gemstone_spellup_script.rb
@@ -3,16 +3,21 @@ require 'scripts/base/base_gemstone_script'
 class BaseGemstoneSpellupScript < BaseGemstoneScript
 
   def run(args)
+    # parse the arguments or set the default(s)
     target = args[0]
+    num_casts = [1, args[1].to_i].max
     # if a target was specified, ensure that they are present
     if target
       return unless hold(target)
     end
+    # process each spell in the sequence
     spells.each do |spell, self_only|
       # skip this spell if a target was specified and the spell is self-cast
       next if target && self_only
-      # ensure that the spell is properly prepped
-      if prep(spell)
+      # handle repeats
+      num_casts.times do |i|
+        # ensure that the spell is prepared
+        break unless prep(spell)
         # cast the spell (if target is nil it will be self-cast)
         cast(target)
         # sleep before proceeding on to the next spell (or exiting)
@@ -26,7 +31,8 @@ class BaseGemstoneSpellupScript < BaseGemstoneScript
   protected
 
   def spells
-    raise %{All "#{BaseGemstoneSpellupScript}(s)" must override the "spells" method}
+    raise \
+      %{All "#{BaseGemstoneSpellupScript}(s)" must override the "spells" method}
   end
 
 end
