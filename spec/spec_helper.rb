c3555d5f48e2e5791969117284f7d29a0e4429a4
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2014-06-08 21:46:16 -0400

    Handle cases where color-output is not available

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,6 @@ require 'rspec'
 
 # configure RSpec
 RSpec.configure do |config|
-  config.color_enabled = true
+  config.color_enabled = true if config.respond_to?(:color_enabled)
   ['./config/environment'].each {|required_module| require required_module}
 end
