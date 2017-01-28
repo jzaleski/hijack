68c5e281bdc617428f6fe3aa710868dee21d5ba5
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2017-01-28 14:31:42 -0500

    Add `to_regexp` to the `Regexp` class for API consistency

--- app/refinements/regexp.rb
+++ app/refinements/regexp.rb
@@ -1,6 +1,12 @@
 class Regexp
+  def to_regexp(opts={})
+    # added for API consistency, effectively a noop
+    self
+  end
+
   def union(*others)
-    # validate that each of the others can be converted to or is a "Regexp" object
+    # validate that each of the `others` are, or can be converted to, `Regexp`
+    # object(s)
     raise 'All other elements must be or be convertable to "Regexp" objects' \
       unless others.all? { |element| element.is_a?(Regexp) || element.is_a?(String) }
     Regexp.union(self, *others)
