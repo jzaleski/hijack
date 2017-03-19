f999edad64221caabb32a088a2cf0799c548d753
Jonathan W. Zaleski <JonathanZaleski@gmail.cm>
2017-03-19 11:08:43 -0400

    Push "moving" state-logic down into `RunnableMovementScriptMixin`

--- app/helpers/script_helper.rb
+++ app/helpers/script_helper.rb
@@ -79,8 +79,6 @@ class ScriptHelper
           until script_names_and_objects.empty?
             # unpack the script-name and script-object
             script_name, script_object = script_names_and_objects.pop
-            # set the `moving` config-var if we're executing a travel script
-            @config[:moving] = true if script_object.respond_to?(:location, true)
             # memoize the script-object so that it can be managed externally
             store(script_name, script_object)
             # update the "last_script" information
@@ -90,8 +88,6 @@ class ScriptHelper
             end
             # start execution then block until the script exits or is killed
             script_object.start_run
-            # reset the `moving` config-var if it is currently set
-            @config.reset(:moving) if @config.include?(:moving)
             # if the "script" was killed, we are done
             break if script_object.killed?
           end
--- app/mixins/base/runnable_movement_script_mixin.rb
+++ app/mixins/base/runnable_movement_script_mixin.rb
@@ -4,8 +4,15 @@ module RunnableMovementScriptMixin
   include MovementScriptMixin
 
   def run
+    # set `moving`
+    @config[:moving] = true
+    # control variable used below to keep track of whether or not we moved
+    # during the current iteration, if the value is `falsy` at any point, after
+    # attempting to move, the script will short-circuit
+    moved = true
+    # `move` all of the `directions` until either the script finishes, is killed
+    # or exits unexpectedly
     directions.each do |direction_or_method|
-      moved = false
       # a method can be passed in for cases where something beyond basic
       # movement is required (e.g. guild/society pass{code,word}s)
       if direction_or_method.respond_to?(:call)
@@ -24,10 +31,12 @@ module RunnableMovementScriptMixin
       end
       # exit the script if there were no valid movement directions or the method
       # returned something falsy
-      return unless moved
+      break unless moved
     end
-    # update location value (we've made it to our destination)
-    @config[:location] = location
+    # reset `moving`
+    @config.reset(:moving)
+    # update `location` if the current value of `moved` is `truthy`
+    @config[:location] = location if moved
   end
 
   protected
