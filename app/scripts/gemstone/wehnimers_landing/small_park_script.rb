df7241d3bd78d0457d668c8911183b0210e69ba5
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2017-08-06 15:24:06 -0400

    Add `SmallPark{,Return}Script(s)` and related tests [GS]

---
+++ app/scripts/gemstone/wehnimers_landing/small_park_script.rb
@@ -0,0 +1,16 @@
+load "#{SCRIPTS_DIR}/gemstone/wehnimers_landing/town_square_central_script.rb", true
+
+class SmallParkScript < TownSquareCentralScript
+  protected
+
+  def directions
+    [
+      SOUTHWEST,
+      SOUTH,
+    ]
+  end
+
+  def location
+    "small_park|#{WEHNIMERS_LANDING}"
+  end
+end
---
+++ spec/scripts/gemstone/wehnimers_landing/small_park_script_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+require 'scripts/gemstone/wehnimers_landing/small_park_script'
+
+describe SmallParkScript do; end
