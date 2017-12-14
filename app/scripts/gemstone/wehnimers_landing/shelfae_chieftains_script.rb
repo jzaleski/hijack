1ece37ff64f767a29ec1e71a45b71a116efda0f9
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2017-12-14 15:23:07 -0500

    Ensure that `ShelfaeChieftains{,Return}Script(s)` go all the way to/from their hunting area

--- app/scripts/gemstone/wehnimers_landing/shelfae_chieftains_script.rb
+++ app/scripts/gemstone/wehnimers_landing/shelfae_chieftains_script.rb
@@ -3,6 +3,16 @@ load "#{SCRIPTS_DIR}/gemstone/wehnimers_landing/shelfae_soldiers_script.rb", tru
 class ShelfaeChieftainsScript < ShelfaeSoldiersScript
   protected
 
+  def directions
+    super + [
+      WEST,
+      WEST,
+      WEST,
+      "go bridge|#{WEST}",
+      "#{WEST}|go bridge",
+    ]
+  end
+
   def location
     "shelfae_chieftains|#{WEHNIMERS_LANDING}"
   end
