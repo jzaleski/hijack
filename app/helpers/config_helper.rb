8b368bffbb1273e36d280d7b5ee92ae9991c10fc
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2017-12-25 20:03:23 -0500

    Switch to JSON for the command-line configuration

--- app/helpers/config_helper.rb
+++ app/helpers/config_helper.rb
@@ -24,21 +24,7 @@ class ConfigHelper
   end
 
   def process_config_file(config_file)
-    ConfigProxy.new.tap do |config|
-      if config_file && File.exist?(config_file)
-        File.new(config_file).each_line do |line|
-          # array or hash property (must be valid JSON)
-          if match_data = /\A(\S+)\s?=\s?(\[.+\]|\{.*\})\Z/.match(line)
-            config[match_data[1].rstrip.gsub(/-/, '_').to_sym] = \
-              JSON::parse(match_data[2].strip, :symbolize_names => true)
-          # basic property
-          elsif match_data = /\A(\S+)\s?=\s?(\S+)\Z/.match(line)
-            config[match_data[1].rstrip.gsub(/-/, '_').to_sym] = \
-              match_data[2].strip
-          end
-        end
-      end
-    end
+    process_hash(JSON.parse(File.read(config_file)))
   end
 
   def process_hash(hash)
