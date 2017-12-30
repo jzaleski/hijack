6c7e74911c0c1061d94a552ef78cde12da07ba45
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2017-12-30 17:41:46 -0500

    Handle another potential retry-response [SIMU]

--- app/scripts/base/base_simutronics_script.rb
+++ app/scripts/base/base_simutronics_script.rb
@@ -3,10 +3,12 @@ load "#{SCRIPTS_DIR}/base/base_script.rb", true
 class BaseSimutronicsScript < BaseScript
   SORRY_YOU_MAY_ONLY_TYPE_AHEAD = 'Sorry, you may only type ahead'
   WAIT = '\.\.\.wait'
+  WAIT_N_SECONDS = 'Wait \d+ second[s]?'
 
   RETRY_PATTERN = [
     SORRY_YOU_MAY_ONLY_TYPE_AHEAD,
     WAIT,
+    WAIT_N_SECONDS,
   ].join('|')
 
   protected
